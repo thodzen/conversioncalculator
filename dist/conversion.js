5ae8baa2d64eaf972cf99cc98e33a6fa0c2539af
thodzen <thodzen@gmail.com>
2019-08-25 23:17:36 -0400

    Update conversion.js

--- dist/conversion.js
+++ dist/conversion.js
@@ -27,4 +27,19 @@ function HexToDec() {
   console.log(decString2);
 }
 
+function BinToHex(){
+  var BtH_input = (document.getElementById("num").value);
+  decString4 = Number.parseInt(BtH_input, 2); // BinToDec
+  decString4 = decString4.toString(16); // DecToHex
+  document.getElementById("answer").innerHTML = decString4;
+  console.log(decString5);
+}
+
+function HexToBin(){
+  var HtB_input = (document.getElementById("num").value);
+  decString5 = Number.parseInt(HtB_input, 16); // HexToDec
+  decString5 = decString5.toString(2); // DecToBin
+  document.getElementById("answer").innerHTML = decString5;
+  console.log(decString5);
+}
 
