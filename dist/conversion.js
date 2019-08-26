1fe0e890ec7dc40268936c8bba5f25a1d0bcf0f7
thodzen <thodzen@gmail.com>
2019-08-25 20:33:21 -0400

    Added answer bar

--- dist/conversion.js
+++ dist/conversion.js
@@ -2,29 +2,29 @@
 function DecToHex() {
   var DtH_input = parseInt(document.getElementById("num").value);
   var hexString = DtH_input.toString(16);
-  alert(hexString);
+  document.getElementById("answer").innerHTML = hexString;
   console.log(hexString);
 }
 
 function DecToBin() {
   var DtB_input = parseInt(document.getElementById("num").value);
   decString3 = DtB_input.toString(2);
+  document.getElementById("answer").innerHTML = decString3;
   console.log(decString3);
-  alert(decString3);
 }
 
 function BinaryToDec() {
   var BtD_input = parseInt(document.getElementById("num").value);  
     decString = Number.parseInt(BtD_input, 2);
+    document.getElementById("answer").innerHTML = decString;
     console.log(decString);
-    alert(decString);  
 }
 
 function HexToDec() {
   var HtD_input = (document.getElementById("num").value);
   decString2 = Number.parseInt(HtD_input, 16);
+  document.getElementById("answer").innerHTML = decString2;
   console.log(decString2);
-  alert(decString2);
 }
 
 
